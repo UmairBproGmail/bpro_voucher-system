94f994e631ec1c56396164fb918703a516b836a7
UmairBproGmail <umairbpro396@gmail.com>
2025-05-26 14:05:33 +0500

    app.py

--- app.py
+++ app.py
@@ -85,8 +85,6 @@ COMPANY_LOGOS = {
 UPLOAD_FOLDER = 'uploads'
 ALLOWED_EXTENSIONS = {'pdf', 'png', 'jpg', 'jpeg', 'gif', 'doc', 'docx'}
 SCOPES = [
-    'openid',
-    'https://www.googleapis.com/auth/userinfo.email',
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/spreadsheets'
 ]
